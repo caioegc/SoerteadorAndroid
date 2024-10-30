e98659d3263ca1ed419161d904842de3dfb0945a
caioegc <dudueocara77@gmail.com>
2024-10-29 21:50:41 -0300

    Hello World!

---
+++ app/src/main/java/com/example/primeiraaplicacao/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.primeiraaplicacao
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.primeiraaplicacao.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    fun sortear(view: View){
+val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random.nextInt(5000)
+        textoResultado.setText("Numero gerado: $numero")
+
+    }
+}
